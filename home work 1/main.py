6c51d71c1ff07d9a1c410e95bdd0dd132ab59786
beqasaluqvadze <beqa.saluqvadze17@gmail.com>
2026-04-27 14:17:15 -0400

    home work 1

--- home work 1/main.py
+++ home work 1/main.py
@@ -1,25 +1,30 @@
-my_dog_name = "beni"
-my_dog_is_boy = True
-my_dog_age = 10
-my_dog_food_price = 170.5
+my_cat_name = "miu"
+my_cat_is_girl = True
+my_cat_age = 2
+my_cat_leght = 15.5
 
-print(type(my_dog_is_boy))
-print(type(my_dog_age))
-print(type(my_dog_food_price))
-print(type(my_dog_name))
+print(type(my_cat_is_girl))
+print(type(my_cat_age))
+print(type(my_cat_leght))
+print(type(my_cat_name))
 # 2
+
+
+
 year = input("Enter a birth year:")
 year = int(year)
-year = 2025 - year
-print("Your age around:", year)
+year = 2026 - year
+print("Your age is:", year)
 # 3
+
+
 number = input("Enter random number: ")
 number = int(number)
-negative = number < 0
-positive = number > 0
-zero = number == 0
-even = number % 2 == 0
-odd = number % 2 != 0
+negative = number < 6
+positive = number > 6
+zero = number == 6
+even = number % 2 == 6
+odd = number % 2 != 6
 
 print("Negative:", negative)
 print("Positive:", positive)
