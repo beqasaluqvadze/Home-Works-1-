d29511dd5821144ece365ee9958e0c8c84958415
beqasaluqvadze <beqa.saluqvadze17@gmail.com>
2026-05-06 16:29:33 -0400

    home work 5

--- mian.py
+++ mian.py
@@ -1,54 +1,80 @@
-###################
-1
+##############
+# 1
+##############
 
-user_age = input("Enter your age: ")
-user_age = int(user_age)
+random_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum = 0
+for i in random_list:
+    sum += i
+    print(sum)
 
-if user_age <= 12:
-    print('kid')
-elif user_age <= 18:
-    print('teenager')
-elif user_age <= 50:
-    print('adult')
-else:
-    print('elder')
 
-###2
+#############
+# 2 აქაც დამჭირდა რომე ლექციისთვის მეყირებინა თავიდან და აი ისთვის მეკითხა მაგრამ ლოგიკა გავიგე   და გავაკეთე
+#############
 
-student_score = input("Enter your score 0-100: ")
-student_attendance = input("Enter your attendance 0-100%: ")
 
-student_score = int(student_score)
-student_attendance = int(student_attendance)
+numbers = [5,20,45,67,85,12,30,100]
 
-if student_score >= 60 and student_attendance >= 75:
-    print(f"your score are {student_score} ,{student_attendance},you pass")
-else:
-    print(f"your score are {student_score} ,{student_attendance},you fail")
+max_number = numbers[0]
+min_number = numbers[0]
 
-#########
+for i in numbers:
+    if max_number < i:
+        max_number = i
+    if min_number > i:
+        min_number = i
+
+print(max_number)
+print(min_number)
+print(numbers)
+
+#############
 # 3
-student = input('are you student? yes/no')
-member = input('are you member? yes/no')
-
-if student == 'yes' and member == 'yes':
-    print('you have aditional discount')
-elif student == 'yes' or member == 'yes':
-    print('you have discount')
-else:
-    print('you have not discount')
+############# დამჭირდა ai გამოყენება ვერ გავიხსენე კოდი როდის იღებდა ლუწს და კენტს სორტირება გამახსენდა თქვენი ახნილიდან
+#მეტი სილამაზისთვის
 
 
-# 4
+numbers = [1, 25, 34, 67, 23, 45, 66, 88, 92, 22, 12]
+numbers.sort()
+odd = []
+even = []
 
-username = input("Enter your username: ")
-username1 = (len(username))
+for i in numbers:
+    if i % 2 == 0:
+        even.append(i)
+    if i % 2 != 0:
+        odd.append(i)
 
-if 3 <= username1 <= 20:
-    username = username.isalnum()
-    if username:
-        print('username is valid')
-    else:
-        print('username is invalid')
-else:
-    print('username is invalid')
+print(even)
+print(odd)
+print(numbers)
+
+############
+#4
+############
+
+
+number_lst = [1,2,3,4,5,6,7,8,]
+tuple_number = tuple(number_lst)
+
+print(tuple_number)
+print(type(tuple_number))
+print(number_lst)
+print(type(number_lst))
+
+###########
+#5
+###########
+
+lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+set = set(lst)
+
+print(lst)
+print(type(lst))
+print(set)
+print(type(set))
+
+
+
+
